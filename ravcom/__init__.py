4fb00565e8563ae6f7202863d14e7beb51165cdd
Kailash <kailash@matelabs.in>
2021-03-16 21:11:05 +0530

    Config updated

--- ravcom/__init__.py
+++ ravcom/__init__.py
@@ -2,6 +2,7 @@ from .db_manager import OpStatus, GraphStatus, ClientOpMappingStatus, Op, Graph,
     ClientOpMapping, DBManager
 from .redis_manager import RavQueue
 from .utils import dump_data, delete_create_database, delete_data_file, save_data_to_file, inform_server
-from .config import QUEUE_LOW_PRIORITY, QUEUE_HIGH_PRIORITY, QUEUE_COMPUTING
+from .config import QUEUE_LOW_PRIORITY, QUEUE_HIGH_PRIORITY, QUEUE_COMPUTING, RDF_MYSQL_HOST, RDF_MYSQL_DATABASE, \
+    RDF_MYSQL_PASSWORD, RDF_MYSQL_USER, RDF_MYSQL_PORT, RDF_REDIS_DB, RDF_REDIS_HOST, RDF_REDIS_PORT
 
 ravcom = DBManager.Instance()
