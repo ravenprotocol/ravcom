9d31446937181685caa77dc017e8d97dec4de741
Kailash <kailash@matelabs.in>
2021-03-16 12:05:48 +0530

    Configurations changed

--- ravcom/config.py
+++ ravcom/config.py
@@ -17,3 +17,4 @@ RDF_MYSQL_DATABASE = os.environ.get("RDF_MYSQL_PASSWORD", "rdf")
 QUEUE_HIGH_PRIORITY = "queue:high_priority"
 QUEUE_LOW_PRIORITY = "queue:low_priority"
 QUEUE_COMPUTING = "queue:computing"
+
